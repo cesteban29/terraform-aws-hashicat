4cd7f70281ead28bc31bc6a8defb88ff3ab8b67b
cesteban29 <85302893+cesteban29@users.noreply.github.com>
2022-11-04 08:14:33 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,6 +10,9 @@ variable "prefix" {
   default = "demo"
 }
 
+variable "region" {
+}
+
 variable "address_space" {
   description = "The address space that is used by the virtual network. You can supply more than one address space. Changing this forces a new resource to be created."
   default     = "10.0.0.0/16"
