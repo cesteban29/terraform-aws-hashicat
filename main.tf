b1c8982542c2a47c029aad3b7720b0f5126de029
cesteban29 <cesteban@hashicorp.com>
2022-11-08 10:57:42 -0600

    attempt to fix file provisioner issue

--- main.tf
+++ main.tf
@@ -139,7 +139,7 @@ resource "null_resource" "configure-cat-app" {
   }
 
   provisioner "file" {
-    source      = "files/"
+    source      = "files/deploy_app.sh"
     destination = "/home/ubuntu/"
 
     connection {
